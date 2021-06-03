651e83f0f3bb52ef2fe3096b7aa350c8176978e7
homayounjafari <75078042+homayounjafari@users.noreply.github.com>
2021-06-03 13:10:58 +0200

    dos and charge density now in one ldos file

--- do_ldos_real_space.py
+++ do_ldos_real_space.py
@@ -25,3 +25,81 @@ from .communication import load_balancing
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
+
+# dos defenition part
+def do_dos ( data_controller, emin, emax, ne, delta ):
+
+  arry,attr = data_controller.data_dicts()
+  bnd = attr['bnd']
+  netot = attr['nkpnts']*bnd
+  emax = np.amin(np.array([attr['shift'], emax]))
+  arry['dos'] = np.empty((ne,), dtype=float)
+  # DOS calculation with gaussian smearing
+  ene = np.linspace(emin, emax, ne)
+
+  if rank == 0 and attr['verbose']:
+    print('Writing DoS Files')
+
+  for ispin in range(attr['nspin']):
+
+    dosaux = np.zeros((ne), order="C")
+
+    E_k = arry['E_k'][:,:bnd,ispin]
+
+    for n in range(ne):
+      dosaux[n] = np.sum(np.exp(-((ene[n]-E_k)/delta)**2))
+
+    dos = np.zeros((ne), dtype=float) if rank == 0 else None
+
+    comm.Reduce(dosaux,dos,op=MPI.SUM)
+    dosaux = None
+
+    if rank == 0:
+      dos *= float(bnd)/(float(netot)*np.sqrt(np.pi)*delta)
+      arry['dos'] = dos
+    fdos = 'dos_%s.dat'%str(ispin)
+    data_controller.write_file_row_col(fdos, ene, dos)
+    data_controller.broadcast_single_array('dos', dtype=float)
+    
+# charge density part    
+    def do_density ( data_controller, nr1, nr2, nr3, internal=False):
+
+  arry,attr = data_controller.data_dicts()
+
+  # Calculation of the electron density
+
+  if rank == 0 and attr['verbose']:
+    print('Writing density files')
+
+  rhoaux = np.zeros((nr1,nr2,nr3,attr['nspin']),dtype=complex,order="C")
+
+  ini_ik,end_ik = load_balancing(comm.Get_size(), rank, attr['nkpnts'])
+  if not 'basis' in arry.keys():
+    if internal:
+      basis,attr['shells'] = build_aewfc_basis(data_controller)
+    else:
+      basis,attr['shells'] = build_pswfc_basis_all(data_controller)
+  else:
+    basis = arry['basis']
+  eps = 1.e-5
+  for ispin in range(attr['nspin']):
+    for ik in range(ini_ik,end_ik):
+      gkspace = calc_gkspace(data_controller,ik,gamma_only=False)
+      atwfcgk = calc_atwfc_k(basis,gkspace,attr['dftSO'])
+      oatwfcgk = ortho_atwfc_k(atwfcgk)
+      atwfcr = fft_allwfc_G2R(oatwfcgk,gkspace, nr1, nr2, nr3, attr['omega'])
+      for nb in range(attr['bnd']):
+        if arry['E_k'][ik-ini_ik,nb,ispin] <= 0.0+eps:
+          tmp = np.tensordot(arry['v_k'][ik-ini_ik,:,nb,ispin],atwfcr[:,:,:,:],axes=(0,0))
+          rhoaux[:,:,:,ispin] += 2*np.conj(tmp)*tmp/attr['nkpnts']*attr['omega']/(nr1*nr2*nr3)
+
+    rho = np.zeros((nr1,nr2,nr3,attr['nspin']),dtype=complex,order="C") if rank == 0 else None
+    
+    comm.Reduce(rhoaux,rho,op=MPI.SUM)
+    rhoaux = None
+
+    if rank == 0:
+      fdensity = attr['outputdir']+'/density_%s.xsf'%str(ispin)
+      write2xsf(data_controller,filename=fdensity,data=np.real(rho[:,:,:,ispin]))
+  if rank == 0:
+    if attr['verbose']: print('Total charge = ',np.real(np.sum(rho)).round(3))
